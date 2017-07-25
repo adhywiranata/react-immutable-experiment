d2c0ef58917344793da107fbc915dedb721ee88c
Adhy <adhywiranatap@gmail.com>
2017-07-25 13:12:26 +0700

    add more test cases for postReducer

--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -5,9 +5,27 @@ import chaiImmutable from 'chai-immutable';
 chai.use(chaiImmutable);
 
 describe('post reducer', () => {
+  it('posts data initial state is defined', () => {
+    const expectedInitialState = postReducer(undefined, { type: '@@REDUX/INIT'});
+    const postsData = expectedInitialState.get('postsData');
+    expect(postsData).to.not.be.undefined;
+  });
+
+  it('isFetching initial state is defined', () => {
+    const expectedInitialState = postReducer(undefined, { type: '@@REDUX/INIT'});
+    const isFetching = expectedInitialState.get('isFetching');
+    expect(isFetching).to.not.be.undefined;
+  });
+
   it('have empty postsData as initial state', () => {
     const expectedInitialState = postReducer(undefined, { type: '@@REDUX/INIT'});
     const postsData = expectedInitialState.get('postsData');
     expect(postsData).to.be.empty;
+  });
+
+  it('have true isFetching as initial state', () => {
+    const expectedInitialState = postReducer(undefined, { type: '@@REDUX/INIT'});
+    const isFetching = expectedInitialState.get('isFetching');
+    expect(isFetching).to.be.true;
   });
 });
